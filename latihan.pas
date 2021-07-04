527e645defb8f3e2a27342f83172e0617ec0f75f
GustiArsyad123 <73971893+GustiArsyad123@users.noreply.github.com>
2021-07-04 20:07:14 +0700

    Add files via upload

---
+++ latihan.pas
@@ -0,0 +1,15 @@
+uses wincrt;
+begin
+writeln('         PROGRAM DATA MABA           ');
+writeln('========================================');
+writeln('|  NIM      |     NAMA      |  JURUSAN |');
+writeln('========================================');
+gotoxy(2,5); write('1310530119');
+gotoxy(14,5); write('GUSTI ARSYAD');
+gotoxy(33,5); write ('S1 TI');
+
+gotoxy(2,6);write('1310530110');
+gotoxy(14,6); write('AZMI APRILIAN');
+gotoxy(33,6); write ('S1 TI');
+end.
+
